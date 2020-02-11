8fbe622626bdda8e229853835ae3495d36114632
Hashmat Ali <hashmatajalali@gmail.com>
2020-02-11 11:11:13 +0500

    added dynamic ports

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var http = require('http');
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World');
-}).listen(process.env.PORT || 3000, "127.0.0.1");
+}).listen(server_port,server_host, "0.0.0.0");
 console.log('Server running at http://127.0.0.1:3000/');
